5282d2aed48bfad8d19a8e91f88138a9bab1dd25
AhmedLoud <ahmed.loudghiri@gmail.com>
2019-02-11 17:04:38 -0600

    tweak

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,10 @@
 import { ITrackingInfo } from './baseTracker';
 import { UPS } from './carriers/ups';
 
-// import { UPS } from "./carriers/ups";
-
-// // const { UPS } = require('./carriers/ups');
-
-// // const options = {
-// //   trackingNumber: '12345'
-// // }
-// // // const ups = new UPS(options);
-// // console.log(options)
-// const upsTracker = new UPS('1ZXA13670390302611');
-
-// upsTracker.getPackageInformation().subscribe(result => {
-//   console.log('result', result);
-// }, (err) => {
-//   console.log('sldkfj', err);
-// })
 
 export const Track = (trackingNumber: string, callback: (infos: ITrackingInfo, err: any) => void): void => {
-  const upsTracker = new UPS(trackingNumber).getPackageInformation().subscribe((infos: ITrackingInfo) => {
+  const upsTracker = new UPS(trackingNumber);
+  upsTracker.getPackageInformation().subscribe((infos: ITrackingInfo) => {
     callback(infos, null);
   }, (err) => {
     callback({} as ITrackingInfo, err);
