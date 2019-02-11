f489d97ebf51ecea12f7b7a1d9a606039c8d903b
AhmedLoud <ahmed.loudghiri@gmail.com>
2019-02-11 17:05:00 -0600

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { ITrackingInfo } from './baseTracker';
 import { UPS } from './carriers/ups';
 
-
 export const Track = (trackingNumber: string, callback: (infos: ITrackingInfo, err: any) => void): void => {
   const upsTracker = new UPS(trackingNumber);
-  upsTracker.getPackageInformation().subscribe((infos: ITrackingInfo) => {
-    callback(infos, null);
-  }, (err) => {
-    callback({} as ITrackingInfo, err);
-  });
+  upsTracker.getPackageInformation().subscribe(
+    (infos: ITrackingInfo) => {
+      callback(infos, null);
+    },
+    err => {
+      callback({} as ITrackingInfo, err);
+    },
+  );
 };
