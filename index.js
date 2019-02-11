1aaac1b4c1c56c0bd9f37cfd79414bef03d2871d
AhmedLoud <ahmed.loudghiri@gmail.com>
2019-02-11 14:54:01 -0600

    Converting to typescript, more to come

--- index.js
+++
@@ -1,7 +0,0 @@
-const { UPS } = require('./carriers/ups');
-
-const options = {
-  trackingNumber: '12345'
-}
-const ups = new UPS(options);
-console.log(ups)
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// const { UPS } = require('./carriers/ups');
+
+// const options = {
+//   trackingNumber: '12345'
+// }
+// // const ups = new UPS(options);
+// console.log(options)
+export const Greeter = (name: string) => `Hello ${name}`;
